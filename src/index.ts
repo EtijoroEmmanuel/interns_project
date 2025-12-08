46162c0468ffc601ec1835c8d7bee34ac03e0d8f
EtijoroEmmanuel <etijoroemmanuel1@gmail.com>
2025-12-08 12:31:06 -0800

    new-configFix

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,34 @@ import app from "./app"
 import { connectDB } from "./db/mongo"
 import { logger } from "./utils/logger"
 
-connectDB().catch((error) => {
-  logger.error(`Failed to connect to database: ${error.message}`)
-})
 
-const PORT = process.env.PORT || 5002
+let isConnected = false;
 
-app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}!!!!`)
-})
+const initializeDB = async () => {
+  if (!isConnected) {
+    try {
+      await connectDB();
+      isConnected = true;
+    } catch (error: any) {
+      logger.error(`Failed to connect to database: ${error.message}`)
+    }
+  }
+};
 
-export default app
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5002
+  
+  connectDB().catch((error) => {
+    logger.error(`Failed to connect to database: ${error.message}`)
+  })
+  
+  app.listen(PORT, () => {
+    logger.info(`Server is running on port ${PORT}!!!!`)
+  })
+}
+
+export default async (req: any, res: any) => {
+  await initializeDB();
+  return app(req, res);
+};
